39d45cdc4e1aa1876e946df1ab3d9479d624e568
angelavivach <fp.avivancodiaz@salesianosdosa.com>
2024-09-25 19:21:56 +0200

    25 sep

---
+++ acceso a datos/ejerFicheros/ejerFicheros/src/ejerIntroduccion/ejer1Recursividad.java
@@ -0,0 +1,79 @@
+package ejerIntroduccion;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ejer1Recursividad {
+	public static void recursividadDirectorios(File directorio) {
+		
+		 if(directorio==null) { //lo igualamos a 1 porque sera el ultimo numero por el que se puede multiplicar
+	    	  System.out.println("Fin");
+	      }else {
+	    	  System.out.println(directorio);
+	    	  
+	      }
+		 return;
+	}
+	public static void crearFichero(File f) {
+		
+		 try {
+		      if(!f.exists()) {
+		            //Creamos fisicamente el fichero
+		           if (f.createNewFile())
+			          System.out.println("Fichero creado");
+		           else
+			          System.out.println("Fichero no ha sido creado");
+		     
+	          }else
+		            System.out.println("El fichero ya existe");
+	
+		            
+       }
+	     catch(IOException e) {
+		     e.printStackTrace();
+	     }
+	}
+	
+	public static void listarFicheros(File d) {
+		File[] ficheros=d.listFiles();
+		
+		for(File f:ficheros) {
+			if(f.isFile()) {
+				System.out.println(f.getName()+" es un fichero");
+			}
+			if(f.isDirectory()) {
+				System.out.println(f.getName()+" es un directorio");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		//crear directorio
+		String nombreDirectorio=".\\ejer1\\";
+		File directorio =new File(nombreDirectorio);
+		// Se escribe el nombre del fichero que se guarda fisicamente
+		String nombrefichero = "ficheroPrueba.txt";
+		File fichero = new File(nombreDirectorio+nombrefichero);
+		
+		
+		if(!directorio.exists()) {
+			System.out.println("el directorio no existe");
+			if(directorio.mkdirs()) {
+			   
+			}else {
+				System.out.println("No se ha podido crear el directorio");
+			}
+			
+		}//Si el directorio no exite 
+		else {
+			//directorio existe
+			System.out.println("El directorio existe");
+			 crearFichero(fichero);
+		}
+		
+		recursividadDirectorios( directorio);
+		listarFicheros(directorio);
+		
+	}
+
+}
